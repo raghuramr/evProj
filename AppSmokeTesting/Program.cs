7b03a899b350718af918a00d2222f491364e7a1b
Harikiran Setty <harikiran.setty@medicalsolutions.com>
2023-12-20 20:23:31 +0530

    Included Comments for Testing

--- AppSmokeTesting/Program.cs
+++ AppSmokeTesting/Program.cs
@@ -9,6 +9,7 @@ namespace AppSmokeTesting
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
+            // This is for Testing
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
